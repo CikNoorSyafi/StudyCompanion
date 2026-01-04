f1420ede2b67d5bdd6fc348c658c543f127c93ca
NOOR AMERA SHAFINAZ BINTI MUHAMMAD AZMI <152240099+CikNoorSyafi@users.noreply.github.com>
2026-01-04 11:40:04 +0800

    first release - initial commit

---
+++ lib/models/login_request.dart
@@ -0,0 +1,9 @@
+import 'user_role.dart';
+
+class LoginRequest {
+  final String username;
+  final String password;
+  final UserRole role;
+
+  LoginRequest({required this.username, required this.password, required this.role});
+}
